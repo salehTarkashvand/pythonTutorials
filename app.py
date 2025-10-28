4e6f111d4b2ad72a3e3041693b5d502f13819320
salehTorkashvand <saleh.tarkashvand@gmail.com>
2025-10-28 17:32:57 +0330

    String Slicing

--- app.py
+++ app.py
@@ -1,5 +1,27 @@
-student_count = 30    #number
-x = 10      # int
-pi = 3.14   #float
-is_published = True   #bolean
-course_name = "python programming"  #string
+
+course = "python programming"  #string
+
+# The code `print(course[0:18])` is slicing the string variable `course` from index 0 up to (but not
+# including) index 18 and then printing the sliced portion of the string.
+print(course[0:18])
+
+# `print(course[0:])` is slicing the string variable `course` starting from index 0 up to the end of
+# the string, and then printing the sliced portion. Since only the start index is specified and no end
+# index is provided, it will slice the string from index 0 to the end of the string and print it.
+print(course[0:])
+
+# `print(course[:])` is slicing the entire string stored in the variable `course` and then printing
+# the sliced portion. In this case, since no start or end index is specified, it will slice the entire
+# string and print it.
+print(course[:])
+
+# `print(course[:-1])` is slicing the string variable `course` from the beginning up to (but not
+# including) the last character, and then printing the sliced portion of the string. This means it
+# will print the entire string except for the last character.
+print(course[:-1])
+
+# `print(course[::2])` is slicing the string variable `course` with a #step# size of 2. This means it
+# will start from the beginning of the string and select every second character, then print the
+# resulting sliced portion.
+print(course[::2])
+
