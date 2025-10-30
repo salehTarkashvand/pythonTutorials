66bdd68aea318bafde6a5978cb2609ab364cc4a6
salehTorkashvand <saleh.tarkashvand@gmail.com>
2025-10-30 16:46:45 +0330

    comparison operators for number & string

--- app.py
+++ app.py
@@ -1,48 +1,53 @@
-# Type conversion
+# comparison operators
 
-## The `int()` function in Python is used for type conversion, specifically for converting a value to
-# an integer data type. In the code snippet you provided, `int(x)` is converting the input value
-# stored in the variable `x` (which is initially a string due to the `input()` function) into an
-# integer. This allows you to perform mathematical operations or comparisons that require integer
-# values.
-int()
-## The `float()` function in Python is used for type conversion, specifically for converting a value
-# to a floating-point number data type. Similar to `int()`, `float()` allows you to convert values to
-# floating-point numbers, which are numbers that have a decimal point. This conversion is useful when
-# you need to perform operations or comparisons that involve decimal values.
-float()
-## The `bool()` function in Python is used for type conversion, specifically for converting a value to
-# a boolean data type. When you pass a value to the `bool()` function, it will return `True` if the
-# value is considered "truthy" and `False` if the value is considered "falsy" based on Python's truth
-# value testing rules.
+# ------------------------- comparison operators for number
 
-#falsy => 0 , "" , none
-#truthy => 1 , "abc" , -2
+# The expression `3 == 3` is using the equality operator `==` to compare if the value on the
+# left side (3) is equal to the value on the right side (3). In this case, it will return `True`
+# because 3 is indeed equal to 3.
+print(3 == 3)
 
-bool()
-# The `str()` function in Python is used for type conversion, specifically for converting a value to a
-# string data type. When you pass a value to the `str()` function, it will convert that value into a
-# string representation. This is useful when you need to concatenate strings, display values as text,
-# or perform operations that require string manipulation.
-str()
+# `3 != 3` is using the inequality operator `!=` to compare if the value on the left side (3) is
+# not equal to the value on the right side (3). In this case, it will return `False` because 3
+# is equal to 3.
+print(3 != 3)
+
+# `3 >= 3` is using the greater than or equal to operator `>=` to compare if the value on the
+# left side (3) is greater than or equal to the value on the right side (3). In this case, it
+# will return `True` because 3 is equal to 3, satisfying the condition of being greater than or
+# equal to 3.
+print(3 >= 3)
+
+# `3 <= 3` is using the less than or equal to operator `<=` to compare if the value on the left
+# side (3) is less than or equal to the value on the right side (3). In this case, it will
+# return `True` because 3 is equal to 3, satisfying the condition of being less than or equal to
+# 3.
+print(3 <= 3)
+
+# `3 < 3` is using the less than operator `<` to compare if the value on the left side (3) is
+# less than the value on the right side (3). In this case, it will return `False` because 3 is
+# not less than 3; they are equal.
+print(3 < 3)
+
+# `3 > 3` is using the greater than operator `>` to compare if the value on the left side (3) is
+# greater than the value on the right side (3). In this case, it will return `False` because 3
+# is not greater than 3; they are equal.
+print(3 > 3)
 
 
+# ------------------------- comparison operators for string
 
-# The `input("x = ")` function is displaying a prompt message "x = " to the user and waiting for
-# the user to enter some input. The input entered by the user will be stored in the variable `x`.
-x = input("x = ")
+# The `ord("a")` function in Python returns the Unicode code point of the character "a". In this
+# case, it will return the integer value 97, which is the Unicode code point for the lowercase
+# letter "a".
+print(ord("a"))
 
+# The expression `"saleh" > "hamed"` is comparing the two strings "saleh" and "hamed" using the
+# greater than operator `>
+print("saleh" > "hamed")
 
-print(# The `type(x)` function in Python is used to determine the data type of the variable `x`. When
-# `type(x)` is called, it returns the type of the value stored in the variable `x`. This is
-# helpful for understanding the nature of the data stored in a variable and can be used for type
-# checking or debugging purposes.
-type(x))
-
-x = int(x)
-
-print(type(x))
-
-y = x + 1
-
-print (f"x ={x} y={y}")
+# The expression `"saleh" == "SALEH"` is using the equality operator `==` to compare if the
+# string "saleh" is equal to the string "SALEH". In this case, it will return `False` because
+# the comparison is case-sensitive in Python, and "saleh" is not equal to "SALEH" due to the
+# difference in letter case.
+print("saleh" == "SALEH")
