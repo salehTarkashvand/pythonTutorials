ccec86da290266229ec8f15dab9af94ae1f302da
salehTorkashvand <saleh.tarkashvand@gmail.com>
2025-10-30 17:10:29 +0330

    Conditions and If statements

--- app.py
+++ app.py
@@ -1,53 +1,39 @@
-# comparison operators
+# Conditions and If statements
 
-# ------------------------- comparison operators for number
+# syntax if statement
 
-# The expression `3 == 3` is using the equality operator `==` to compare if the value on the
-# left side (3) is equal to the value on the right side (3). In this case, it will return `True`
-# because 3 is indeed equal to 3.
-print(3 == 3)
-
-# `3 != 3` is using the inequality operator `!=` to compare if the value on the left side (3) is
-# not equal to the value on the right side (3). In this case, it will return `False` because 3
-# is equal to 3.
-print(3 != 3)
-
-# `3 >= 3` is using the greater than or equal to operator `>=` to compare if the value on the
-# left side (3) is greater than or equal to the value on the right side (3). In this case, it
-# will return `True` because 3 is equal to 3, satisfying the condition of being greater than or
-# equal to 3.
-print(3 >= 3)
-
-# `3 <= 3` is using the less than or equal to operator `<=` to compare if the value on the left
-# side (3) is less than or equal to the value on the right side (3). In this case, it will
-# return `True` because 3 is equal to 3, satisfying the condition of being less than or equal to
-# 3.
-print(3 <= 3)
-
-# `3 < 3` is using the less than operator `<` to compare if the value on the left side (3) is
-# less than the value on the right side (3). In this case, it will return `False` because 3 is
-# not less than 3; they are equal.
-print(3 < 3)
-
-# `3 > 3` is using the greater than operator `>` to compare if the value on the left side (3) is
-# greater than the value on the right side (3). In this case, it will return `False` because 3
-# is not greater than 3; they are equal.
-print(3 > 3)
+#if Conditions:
+#    Operation
+#elif Conditions:
+#    Operation
+#elif Conditions:
+#    Operation
+#else:
+#    Operation
 
 
-# ------------------------- comparison operators for string
+# This code snippet is using conditional statements to determine the appropriate message to print
+# based on the value of the `temperature` variable. Here's a breakdown of the logic:
 
-# The `ord("a")` function in Python returns the Unicode code point of the character "a". In this
-# case, it will return the integer value 97, which is the Unicode code point for the lowercase
-# letter "a".
-print(ord("a"))
+temprature = 35
+# `if temperature > 30:` is a conditional statement that checks if the value stored in the
+# `temperature` variable is greater than 30. If this condition is true, the code block under this `if`
+# statement will be executed, which in this case is printing "it's warm" and "drink water".
+if temprature > 30:
+    print("it`s warm")
+    print("drink water")
 
-# The expression `"saleh" > "hamed"` is comparing the two strings "saleh" and "hamed" using the
-# greater than operator `>
-print("saleh" > "hamed")
+# `elif temperature > 20:` is an "else if" statement that checks if the value stored in the
+# `temperature` variable is greater than 20. If the previous `if` condition (temperature > 30) is not
+# met, this `elif` condition is evaluated. If the temperature is greater than 20 but not greater than
+# 30, the code block under this `elif` statement will be executed, which in this case is printing
+# "it's nice".
+elif temprature > 20:
+    print("it`s nice")
 
-# The expression `"saleh" == "SALEH"` is using the equality operator `==` to compare if the
-# string "saleh" is equal to the string "SALEH". In this case, it will return `False` because
-# the comparison is case-sensitive in Python, and "saleh" is not equal to "SALEH" due to the
-# difference in letter case.
-print("saleh" == "SALEH")
+# The `else:` statement in Python is used as a catch-all condition that executes when none of the
+# preceding `if` or `elif` conditions are true. In the context of the code snippet you provided, if
+# the temperature is not greater than 30 and not greater than 20, then the `else:` block will be
+# executed, which prints "it's cold".
+else:
+    print("it`s cold")
