a9490baa1f6e2e3bc07515409891adc4eee12f87
salehTorkashvand <saleh.tarkashvand@gmail.com>
2025-10-30 13:24:07 +0330

    formatted string

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-# \"
-# \'
-# \\
-# \n
+# formatted string
 
-course = "python programming"  #escape notation
+name = "saleh"
+lastName = "tarkashvand"
 
+# This line of code is using a formatted string in Python. It is creating a new string variable `full`
+# by combining the values of the `name` and `lastName` variables with a space in between. The `f`
+# before the string indicates that it is a formatted string, allowing you to embed expressions inside
+# curly braces `{}` within the string.
 
-
+full = f"{name} {lastName}"
