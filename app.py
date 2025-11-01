6d794660d1bfe0d9c1da4764969584d92718da1c
salehTorkashvand <saleh.tarkashvand@gmail.com>
2025-11-01 21:57:20 +0330

    logical operators (and , or , not )

--- app.py
+++ app.py
@@ -1,12 +1,48 @@
-# ternary operator 
+# logical operators
 
-age = 15
+#and
+#or
+#not
 
-# This line of code is using a ternary operator in Python. It is checking if the variable `age` is
-# greater than or equal to 18. If the condition is true, it assigns the value "eligible" to the
-# variable `message`. If the condition is false, it assigns the value "not eligible" to the variable
-# `message`. This is a concise way of writing an if-else statement in a single line.
+good_credit = False
+high_income = True
+student = False
 
-message = "eligible" if age >= 18 else "not eligible"
+# The `or` operator in Python is a logical operator that returns `True` if at least one
+# of the conditions it connects evaluates to `True`. In your code snippet, the
+# expression `good_credit or high_income` will evaluate to `True` if either
+# `good_credit` is `True`, `high_income` is `True`, or both are `True`. If both
+# `good_credit` and `high_income` are `False`, then the `or` expression will evaluate
+# to `False`.
 
-print(message)
+if good_credit or high_income:
+    print("eligibale")
+else:
+    print("not eligibale")
+
+# The `and` operator in Python is a logical operator that returns `True` only if both
+# of the conditions it connects evaluate to `True`. In your code snippet, the
+# expression `good_credit and high_income` will evaluate to `True` only if both
+# `good_credit` and `high_income` are `True`. If either `good_credit` or `high_income`
+# is `False`, then the `and` expression will evaluate to `False`.
+
+if good_credit and high_income:
+    print("eligibale")
+else:
+    print("not eligibale")
+
+# The `not` operator in Python is a logical operator that negates the boolean value of the operand
+# it precedes. In your code snippet, the expression `not student` will evaluate to `True` if
+# `student` is `False`, and `False` if `student` is `True`. It essentially flips the boolean value
+# of the operand.
+if not student:
+    print("eligibale")
+else:
+    print("not eligibale")
+
+# The expression `not student and (good_credit and high_income)` is combining multiple conditions
+# using logical operators in Python.
+if not student and ( good_credit and high_income ):
+    print("eligibale")
+else:
+    print("not eligibale")
