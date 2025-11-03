0c4b3dc189698cc1bb94c4e69301a5c8af00386f
salehTorkashvand <saleh.tarkashvand@gmail.com>
2025-11-03 14:26:52 +0330

    for - else

--- app.py
+++ app.py
@@ -1,19 +1,11 @@
-# for operator
+# for - else
 
-# for
+successful = False
 
-# The `for` loop in Python is iterating over a range of numbers from 0 to 3 (exclusive) using the
-# `range(4)` function. Inside the loop, it is printing the message "message" and then printing the
-# current number in the iteration. This loop will execute 4 times, with `number` taking on the values
-# 0, 1, 2, and 3 in each iteration.
-
-
-# in
-
-# The `in` keyword in this context is used to iterate over the elements of the iterable
-# object `range(4)`. It assigns each element of the iterable to the variable `number` in
-# each iteration of the loop.
-
-for number in range(4):
-    print("message")
-    print(number)
+for number in range(3):
+    print("attempt")
+    if(successful):
+        print("successful")
+        break
+else:
+    print("attempt 3 time and faild")
