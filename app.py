f0356b418b39ceb683b9b73bd3d75b545e8b5757
salehTorkashvand <saleh.tarkashvand@gmail.com>
2025-11-04 14:08:38 +0330

    unpack and enumerate

--- app.py
+++ app.py
@@ -1,24 +1,16 @@
-# list unpack
+# unpack and enumerate
 
-list = [1, 2, 3]
-a, b, c = list
+letters = ["a", "b", "c"]
+for letter in enumerate(letters):
+    print(letter) # tuple
+# (0, 'a')
+# (1, 'b')
+# (2, 'c')
 
-print(a, b, c)  # 1 2 3
-
-
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-a, b, *c = numbers
-
-print(a, b, c)  # 1 2 [3, 4, 5, 6, 7, 8, 9]
-
-
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-a, *c, b = numbers
-
-print(a, b, c)  # 1 9 [2, 3, 4, 5, 6, 7, 8]
-
-
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-a, *c, b, d = numbers
-
-print(a, b, c, d)  # 1 8 [2, 3, 4, 5, 6, 7] 9
+#iterate with unpack
+letters = ["a", "b", "c"]
+for index , letter in enumerate(letters):
+    print(index) # tuple
+# 0
+# 1
+# 2
