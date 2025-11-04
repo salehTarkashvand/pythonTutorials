436974db5560a0e6ef482d607af5fc048de45b71
salehTorkashvand <saleh.tarkashvand@gmail.com>
2025-11-04 14:39:24 +0330

    add and remove from list

--- app.py
+++ app.py
@@ -1,16 +1,43 @@
-# unpack and enumerate
+# add and remove from list
 
-letters = ["a", "b", "c"]
-for letter in enumerate(letters):
-    print(letter) # tuple
-# (0, 'a')
-# (1, 'b')
-# (2, 'c')
 
-#iterate with unpack
+                #add
+
+#append
 letters = ["a", "b", "c"]
-for index , letter in enumerate(letters):
-    print(index) # tuple
-# 0
-# 1
-# 2
+
+letters.append("b")
+print(letters)  # ['a', 'b', 'c', 'b']
+
+#insert
+letters = ["a", "b", "c"]
+
+letters.insert(0,"b")
+print(letters)  # ['b', 'a', 'b', 'c']
+
+            #remove
+
+#pop
+letters = ["a", "b", "c"]
+
+letters.pop()
+print(letters)  # ['a', 'b']
+
+#pop(index)
+letters = ["a", "b", "c"]
+
+letters.pop(0)
+print(letters)  # ['b', 'c']
+
+#remove
+letters = ["a", "b", "c"]
+
+letters.remove("c")
+print(letters)  # ['a', 'b']
+
+#del letters[index or :2 ]
+letters = ["a", "b", "c"]
+del letters[2] #['a', 'b']
+del letters[:2] #['c']
+print(letters)
+
